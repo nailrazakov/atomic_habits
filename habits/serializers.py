50a56c514f1161e2b9482828651c9957494805c6
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-11-16 18:05:03 +0300

    validation

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from habits.models import Habit
+from habits.validators import (
+    ChoiceRewardValidator,
+    TimeValidator,
+    RelatedHabitValidator,
+    PeriodicityValidator,
+    NoConnectionValidator,
+)
 
 
 class HabitSerializer(ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
+        validators = [
+            ChoiceRewardValidator("related_habit", "reward"),
+            TimeValidator("time_to_complete"),
+            RelatedHabitValidator(),
+            PeriodicityValidator("periodicity"),
+            NoConnectionValidator("pleasant_habit", "related_habit", "reward"),
+        ]
